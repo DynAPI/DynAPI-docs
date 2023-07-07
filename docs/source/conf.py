a4a5f13e0df05743f7fb2d9fe763ed2d7c71b759
Konstantin Lobmayr <s_lobmayr21@stud.hwr-berlin.de>
2023-07-07 11:23:54 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DynAPI'
+copyright = '2021, GameplayerG9|Konstantinlob'
+author = 'GameplayerG9|Konstantinlob'
 
 release = '0.1'
 version = '0.1.0'
